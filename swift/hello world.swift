c70ce1d023b61b9eeb3b0b02cdf7f3b18b35e535
chipset <higorfrancisco1416@gmail.com>
2023-08-27 22:13:51 -0300

    atualizado

--- swift/hello world.swift
+++ swift/hello world.swift
@@ -1,1 +1,23 @@
-print("hello world")
+import Foundation
+
+print("Turno")
+if let turno = readLine(), !turno.isEmpty {
+    print("Salario")
+    if let salarioInput = readLine(), let salarioValue = Double(salarioInput) {
+        let inss = (salarioValue / 100) * 11
+        let fgts = (salarioValue / 100) * 8
+        let recebe = salarioValue - fgts - inss
+
+        print("Receber \(recebe)")
+
+        if recebe >= 1201 {
+            print("Receber em TED")
+        } else {
+            print("Receber em PIX")
+        }
+    } else {
+        print("Salário inválido")
+    }
+} else {
+    print("Turno inválido")
+}
